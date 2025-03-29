cb2939d61903ea5f0964136f3fb43a10209f5d8d
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 18:36:50 -0300

    refactor: update create_consumer endpoint to use ConsumerCreate schema

--- controllers/ConsumerController.py
+++ controllers/ConsumerController.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.database import get_db
 from models.models import Consumer
+from schemas.consumer import ConsumerCreate
 from daos.ConsumerDao import ConsumerDAO
 
 router = APIRouter(prefix="/consumers", tags=["consumers"])
 
-@router.post("/", response_model=Consumer)
-def create_consumer(consumer: Consumer, db: Session = Depends(get_db)):
+@router.post("/", response_model=ConsumerCreate)
+def create_consumer(consumer_data: ConsumerCreate, db: Session = Depends(get_db)):
     dao = ConsumerDAO(db)
+    consumer = Consumer(**consumer_data.model_dump())
     return dao.create(consumer)
--- daos/consumerDao.py
+++ daos/consumerDao.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.future import select
 from fastapi import Depends
 from database.database import get_db
-from models import Consumer
+from models.models import Consumer
 
 class ConsumerDAO:
     def __init__(self, db: Session = Depends(get_db)):
