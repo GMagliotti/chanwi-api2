ee72756e4be87bc5f4809be3e32d7f995bc4fb01
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 20:44:49 -0300

    feat: add CORS middleware to allow cross-origin requests

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import ConsumerController,receiverController, ProducerController, driveController, orderController, postController
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods (GET, POST, PUT, DELETE, OPTIONS)
+    allow_headers=["*"], # Allows all headers
+)
+
+
 app.include_router(ConsumerController.router, tags=["consumers"])
 app.include_router(receiverController.router, tags=["receivers"])
 app.include_router(ProducerController.router, tags=["producers"])
