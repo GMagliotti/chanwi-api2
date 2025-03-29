7860837a364a2b14f20d004240eab26d102d3886
NCasella <nicolascasella02@gmail.com>
2025-03-29 15:54:09 -0300

    fix: added endpoints to routing

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
-from controllers import producerController
+from controllers import producerController,postController,orderController,receiverController
 
 app = FastAPI()
-app.include_router(producerController.router)
 
+app.include_router(producerController.router, prefix="/producer", tags=["producer"])
+app.include_router(postController.router, prefix="/post", tags=["post"])
+app.include_router(orderController.router, prefix="/order", tags=["order"])
+app.include_router(receiverController.router, prefix="/receivers", tags=["receiver"])
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
