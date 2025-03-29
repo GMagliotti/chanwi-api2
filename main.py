c422918200ed7a46e2bce3a2ad0420334a75d1d7
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 19:40:46 -0300

    chome sanges

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import Union
 
 from fastapi import FastAPI
-from controllers import ConsumerController,receiverController, ProducerController
+from controllers import ConsumerController,receiverController, ProducerController, driveController, orderController, postController
 
 app = FastAPI()
 app.include_router(ConsumerController.router, tags=["consumers"])
 app.include_router(receiverController.router, tags=["receivers"])
 app.include_router(ProducerController.router, tags=["producers"])
+app.include_router(driveController.router, tags=["drives"])
+app.include_router(orderController.router, tags=["orders"])
+app.include_router(postController.router, tags=["posts"])
