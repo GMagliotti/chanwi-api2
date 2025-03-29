be7f1ab69a0153421bab37a4b5cedf4e44f6d9ca
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 17:44:02 -0300

    refactor: reorganized router inclusions and added drive and consumer controllers

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
-from controllers import producerController,postController,receiverController
+from controllers import producerController,postController,receiverController, driveController, consumer_controller #, orderController
 
 app = FastAPI()
 
-app.include_router(producerController.router, prefix="/producer", tags=["producer"])
-app.include_router(postController.router, prefix="/post", tags=["post"])
-#app.include_router(orderController.router, prefix="/order", tags=["order"])
-app.include_router(receiverController.router, prefix="/receivers", tags=["receiver"])
+app.include_router(producerController.router)
+app.include_router(driveController.router)
+app.include_router(receiverController.router)
+app.include_router(consumer_controller.router)
+app.include_router(postController.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
