0b89f2b74a1885e0eb8ca766d954c77b64856e02
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 12:27:31 -0300

    update database connection string for local development

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://user:password@localhost/dbname?client_encoding='utf8mb4'"
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/chanwi-db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
