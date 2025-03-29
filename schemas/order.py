337915ab41b649bbb3a8ee77ffe689819cf3b058
NCasella <nicolascasella02@gmail.com>
2025-03-29 19:11:37 -0300

    feat: added order endpoints

---
+++ schemas/order.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# Schema for creating an order
+class CreateOrderRequest(BaseModel):
+    order_id: int
+    quantity: int
+    consumer_id: int
+    post_id: int
+    received: bool
+
+# Schema for getting orders
+class GetOrdersRequest(BaseModel):
+    post_id: Optional[int] = None
+    consumer_id: Optional[int] = None
+class Order(BaseModel):
+    id: int
+    quantity: int
+    consumer_id: int
+    post_id: int
+    received: bool
+
+    class Config:
+        orm_mode = True
