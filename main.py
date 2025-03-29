686691bb3a8ed8d07e00eb088a8689edde4c2095
NCasella <nicolascasella02@gmail.com>
2025-03-29 16:33:49 -0300

    reformat: removed orderController occurrences

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union
 
 from fastapi import FastAPI
-from controllers import producerController,postController,orderController,receiverController
+from controllers import producerController,postController,receiverController
 
 app = FastAPI()
 
 app.include_router(producerController.router, prefix="/producer", tags=["producer"])
 app.include_router(postController.router, prefix="/post", tags=["post"])
-app.include_router(orderController.router, prefix="/order", tags=["order"])
+#app.include_router(orderController.router, prefix="/order", tags=["order"])
 app.include_router(receiverController.router, prefix="/receivers", tags=["receiver"])
 @app.get("/")
 def read_root():
