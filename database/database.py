fcbd64f201d6a7d0547f071b3495c50d36867200
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-03-29 14:10:39 -0300

    feat: configure database connection and add development/production docker-compose files

--- database/database.py
+++ database/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost/chanwi-db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@localhost/chanwi-db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
